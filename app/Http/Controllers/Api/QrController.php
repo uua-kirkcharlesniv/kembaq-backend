537758ee3815d3765c1791207c26e85233a982ee
Kirk Charles Niverba <kirkniverba23@gmail.com>
2022-05-10 20:51:15 +0800

    qr fixes

--- app/Http/Controllers/Api/QrController.php
+++ app/Http/Controllers/Api/QrController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Merchant;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
@@ -27,18 +28,26 @@ class QrController extends Controller
     }
 
     public function generateQrCodeMerchant(Request $request) {
-        request()->validate([
-            'merchant_id' => 'required|exists:merchants,id',
-        ]);
+        if(!$request->headers->has('user_id')) {
+            return response()->json([
+                'message' => 'Merchant ID is required.'
+            ], 400);
+        }
+
+        if(!$request->headers->has('value')) {
+            return response()->json([
+                'message' => 'Value is required.'
+            ], 400);
+        }
 
         $validity = Carbon::now('UTC')->addSeconds(180);
-        $message = 'id:'.$request->merchant_id.';expires_at:'.$validity->getTimestampMs();
+        $merchant = Merchant::findOrFail($request->header('user_id'));
+        $message = 'id:'.$merchant->id.';expires_at:'.$validity->getTimestampMs();
+        $message .= ';value:'.$request->header('value');
         $checksum = crc32($message);
-        $message = $message.';checksum:'.$checksum;
+        $message .= ';checksum:'.$checksum;
         
-        return response()->json([
-            'data' => QrCode::size(800)->format('png')->style('dot')->eye('circle')->errorCorrection('H')->merge($request->get('merchant')->logo, .3, true)->generate($message),
-        ]);
+        return response(QrCode::size(800)->format('png')->errorCorrection('H')->merge($merchant->logo, .3, true)->generate($message));
     }
 
     public function validateQrCodeUser(Request $request) {
@@ -75,22 +84,29 @@ class QrController extends Controller
         request()->validate([
             'id' => 'required|exists:merchants,id',
             'expires_at' => 'required|numeric',
+            'value' => 'required|numeric',
             'checksum' => 'required|numeric',
         ]);
 
-        $reconstructed = 'id:'.$request->id.';expires_at:'.$request->expires_at;
+        $reconstructed = 'id:'.$request->id.';expires_at:'.$request->expires_at.';value:'.$request->value;
         $reconstructedChecksum = crc32($reconstructed);
         if($request->checksum != $reconstructedChecksum) {
-            return response('Invalid checksum.', 400);
+            return response()->json([
+                'message' => 'Cannot validate the authenticity of the QR code.'
+            ], 400);
         }
 
         $currentTimeUTC = Carbon::now('UTC');
         $expiryDate = Carbon::createFromTimestampMsUTC($request->expires_at);
         
         if($currentTimeUTC->greaterThanOrEqualTo($expiryDate)) {
-            return response('Code already expired.', 400);
+            return response()->json([
+                'message' => 'Code already expired.'
+            ], 400);
         }
 
-        return response('OK.', 200);
+        return response()->json([
+            'OK.'
+        ], 200);
     }
 }
