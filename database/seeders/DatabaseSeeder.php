242b662c243ee2496e8d01c655af1272a9dd1f0c
Kirk Charles Niverba <kirkniverba23@gmail.com>
2022-07-22 04:42:26 +0800

    only seed categories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -39,67 +39,67 @@ class DatabaseSeeder extends Seeder
             'name'=> 'Clinic',
         ]);
 
-        $kcU = User::create([
-            'first_name' => 'Kirk Charles',
-            'last_name' => 'Niverba',
-            'email' => 'kirkniverba@icloud.com',
-            'password' => Hash::make('password'),
-        ]);
+        // $kcU = User::create([
+        //     'first_name' => 'Kirk Charles',
+        //     'last_name' => 'Niverba',
+        //     'email' => 'kirkniverba@icloud.com',
+        //     'password' => Hash::make('password'),
+        // ]);
 
-        User::create([
-            'first_name' => 'Tony Tan',
-            'last_name' => 'Caktiong',
-            'email' => 'tonytan.caktiong@jollibee.com',
-            'password' => Hash::make('password'),
-            'is_merchant' => true,
-        ]);
+        // User::create([
+        //     'first_name' => 'Tony Tan',
+        //     'last_name' => 'Caktiong',
+        //     'email' => 'tonytan.caktiong@jollibee.com',
+        //     'password' => Hash::make('password'),
+        //     'is_merchant' => true,
+        // ]);
 
-        $mc = Merchant::create([
-            'mobile_number' => '09451494339',
-            'logo' => 'http://assets.stickpng.com/images/62306f7fa39b9e9c515e5925.png',
-            'background_color' => 'ED202A',
-            'button_color' => 'FA8003',
-            'business_address' => 'Bayan-Bayanan Ave, corner Molave St, Marikina, Metro Manila',
-            'lat' => 14.656098220476428,
-            'long' => 121.11899409976772,
-            'business_name' => 'Jollibee - Marikina',
-            'category' => 1,
-        ]);
+        // $mc = Merchant::create([
+        //     'mobile_number' => '09451494339',
+        //     'logo' => 'http://assets.stickpng.com/images/62306f7fa39b9e9c515e5925.png',
+        //     'background_color' => 'ED202A',
+        //     'button_color' => 'FA8003',
+        //     'business_address' => 'Bayan-Bayanan Ave, corner Molave St, Marikina, Metro Manila',
+        //     'lat' => 14.656098220476428,
+        //     'long' => 121.11899409976772,
+        //     'business_name' => 'Jollibee - Marikina',
+        //     'category' => 1,
+        // ]);
 
-        Message::create([
-            'merchant_id' => 1,
-            'title' => 'Welcome to Jollibee',
-            'link' => 'https://www.jollibeedelivery.com/',
-            'message' => 'Lorem ipsum dolor sit amet, consectadipiscing elit...'
-        ]);
+        // Message::create([
+        //     'merchant_id' => 1,
+        //     'title' => 'Welcome to Jollibee',
+        //     'link' => 'https://www.jollibeedelivery.com/',
+        //     'message' => 'Lorem ipsum dolor sit amet, consectadipiscing elit...'
+        // ]);
 
-        DB::table('merchant_user')->insert([
-            ['merchant_id' => 1, 'user_id' => 2]
-        ]);
+        // DB::table('merchant_user')->insert([
+        //     ['merchant_id' => 1, 'user_id' => 2]
+        // ]);
 
-        $jdc = User::create([
-            'first_name' => 'Juan',
-            'last_name' => 'dela Cruz',
-            'email' => 'juandelacruz@moveup.app',
-            'password' => Hash::make('password'),
-        ]);
+        // $jdc = User::create([
+        //     'first_name' => 'Juan',
+        //     'last_name' => 'dela Cruz',
+        //     'email' => 'juandelacruz@moveup.app',
+        //     'password' => Hash::make('password'),
+        // ]);
 
-        Reward::create([
-            'merchant_id' => $mc->id,
-            'title' => 'Sample reward',
-            'description' => 'Lorem ipsum generic description',
-            'value' => 50,
-            'photo' => 'https://cdn.shopify.com/s/files/1/0580/3245/5858/files/CJ_Fam_Deals.jpg?v=1643830712&width=1080',
-            'valid_until' => Carbon::now()->utc()->addDays(30),
-        ]);
+        // Reward::create([
+        //     'merchant_id' => $mc->id,
+        //     'title' => 'Sample reward',
+        //     'description' => 'Lorem ipsum generic description',
+        //     'value' => 50,
+        //     'photo' => 'https://cdn.shopify.com/s/files/1/0580/3245/5858/files/CJ_Fam_Deals.jpg?v=1643830712&width=1080',
+        //     'valid_until' => Carbon::now()->utc()->addDays(30),
+        // ]);
 
-        Reward::create([
-            'merchant_id' => $mc->id,
-            'title' => 'Sample reward 2',
-            'description' => 'Lorem ipsum generic description',
-            'value' => 5,
-            'photo' => 'https://cdn.shopify.com/s/files/1/0580/3245/5858/files/CJ_Fam_Deals.jpg?v=1643830712&width=1080',
-            'valid_until' => Carbon::now()->utc()->addDays(15),
-        ]);
+        // Reward::create([
+        //     'merchant_id' => $mc->id,
+        //     'title' => 'Sample reward 2',
+        //     'description' => 'Lorem ipsum generic description',
+        //     'value' => 5,
+        //     'photo' => 'https://cdn.shopify.com/s/files/1/0580/3245/5858/files/CJ_Fam_Deals.jpg?v=1643830712&width=1080',
+        //     'valid_until' => Carbon::now()->utc()->addDays(15),
+        // ]);
     }
 }
